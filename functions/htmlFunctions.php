54267bd893ba779886e591f62f2f083a89262de1
maxallen <maxallen1234@gmail.com>
2023-09-10 01:17:13 -0400

    scoreboard fix

--- functions/htmlFunctions.php
+++ functions/htmlFunctions.php
@@ -30,7 +30,7 @@ function pageHeader($dbConn, $pageTitle) {
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css" integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 			<link href="css/pickem.css?version=2" rel="stylesheet">
-			<link href="css/scoreboard.css?version=2" rel="stylesheet">
+			<link href="css/scoreboard.css?version=3" rel="stylesheet">
 			<link href="css/schoolColors.php?version=2" rel="stylesheet">
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 			<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
